68ce11289e667af9bc4516b14474b4dcae11e08a
Ragsomar <demarspierrealban@gmail.com>
2020-02-28 17:18:59 +0100

    Delete Nexus component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import Nexus from './components/Nexus'
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
-      <Nexus />
+      <div>Hello World</div>
     </div>
   );
 }
